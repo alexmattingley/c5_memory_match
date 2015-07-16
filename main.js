6da025e1cfc358c63c27e6ec5a53545f27140061
Alex Mattingley <alexmattingley@gmail.com>
2015-07-16 11:03:51 -0700

    Ok, comparing card values, lets see what dan says

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 var card_back_id = null;
 var card_front_id = null;
+var next_card_is_second_click = false;
 function card_click(card_back_id, card_front_id) {
 	$(card_back_id).hide();
 	var card_src = $(card_front_id).attr('src');
-	console.log('the image\'s source is ' + card_src);
-	if (card_back_id == '#card_back1') {
-		var card_src_one = $(card_front_id).attr('src');
-		console.log('that is the first card');
+	// console.log('the image\'s source is ' + card_src);
+	
+	if(next_card_is_second_click == false) {
+		//I am at the first card clicked
+		card_src_one = card_src;
+		console.log('I am at the first card clicked and my source is ' + card_src_one);
+		next_card_is_second_click = true;
+		
 	}
 	else {
-		console.log('that is not the first card');
+		card_src_two = card_src;
+		console.log('I am at the second card clicked and my source is ' + card_src_two);
+		if (card_src_one == card_src_two) {
+			console.log('My cards srcs match and they shouldnt. do it again');
+		}
+		else {
+			console.log('try again');
+		}
 	}
 	
 }
