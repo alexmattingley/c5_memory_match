ac4f8309f884be3216d9f95173cbb4b87a4f5b93
Alex Mattingley <alexmattingley@gmail.com>
2015-07-16 07:53:04 -0700

    getting close to .5.3, identified srcs correctly, now for the if statement

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // So if I pass in a variable of the id, I can select the individual cards
 var card_back_id = null;
-function card_click(card_back_id) {
+var card_front_id = null;
+function card_click(card_back_id, card_front_id) {
 	$(card_back_id).hide();
+	var card_src = $(card_front_id).attr('src');
+	console.log('the first image\'s source is ' + card_src);
 }
