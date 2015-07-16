9e622c1024231bb0adba14c35a9ec7c27a7f855a
Alex Mattingley <alexmattingley@gmail.com>
2015-07-16 07:20:39 -0700

    version .5.2 complete

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-function card_click() {
-	$('#card_back1').hide();
+// So if I pass in a variable of the id, I can select the individual cards
+var card_back_id = null;
+function card_click(card_back_id) {
+	$(card_back_id).hide();
 }
